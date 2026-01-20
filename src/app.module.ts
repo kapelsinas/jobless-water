98b5a6fcf0da8822a0d0981b2f81299040b2c4d7
karolis <karka41@gmail.com>
2026-01-20 19:04:17 +0200

    feat: add config module, setup docker and TypeORM database, remove unused init files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { appConfig, AppConfig } from './config/app.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const dbConfig =
+          configService.get<AppConfig['database']>('app.database');
+
+        return {
+          type: 'mysql',
+          host: dbConfig?.host,
+          port: dbConfig?.port,
+          username: dbConfig?.username,
+          password: dbConfig?.password,
+          database: dbConfig?.name,
+          autoLoadEntities: true,
+          synchronize: true, // Only for development
+        };
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
